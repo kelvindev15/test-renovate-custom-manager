51601acaa4759ee95a1dc9db1ca45ea790b4ebe0
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2024-08-04 10:26:25 +0200

    chore: add some deps for renovate

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,10 +8,11 @@ packageJson {
     description = "My App"
     main = "index.js"
     scripts {
-        "start" runs "node build/js/packages/my-app/kotlin/my-app.js"
+        script("start" runs "node build/js/packages/my-app/kotlin/my-app.js")
     }
     dependencies {
         "react" version "17.0.2"
         "react-dom" version "17.0.2"
+        "@zuccherosintattico/events".version("8.1.0")
     }
 }
