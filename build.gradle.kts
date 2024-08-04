25f09fc95509dd20397dbb78bead560a43680503
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2024-08-04 10:11:20 +0200

    chore: add files

---
+++ build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("io.github.kelvindev15.npm-gradle-plugin") version "3.1.0"
+}
+
+packageJson {
+    name = "my-app"
+    version = "1.0.0"
+    description = "My App"
+    main = "index.js"
+    scripts {
+        "start" runs "node build/js/packages/my-app/kotlin/my-app.js"
+    }
+    dependencies {
+        "react" version "17.0.2"
+        "react-dom" version "17.0.2"
+    }
+}
